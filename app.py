f470b4fdd76ca42d7a670fa3863de6ecb8395ef4
Leo Varin <leo.varin@telecom-st-etienne.fr>
2024-04-04 16:06:30 +0200

    add RCNN

--- app.py
+++ app.py
@@ -2,13 +2,27 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title(' Cloud - Résultats metrics view')
-    df = pd.readcsv('data.csv')
-    st.write("Accurancy per epoch")
-    st.linechart(data=df, x='epoch', y='accuracy')
-    st.write("Mean Iou per epoch")
-    st.linechart(data=df, x='epoch', y='mean_iou')
+    st.title('Cloud - Résultats metrics view')
+    df = pd.read_csv('data.csv')
+    
+    st.write("Accuracy per epoch")
+    filtered_df = df[(df['accuracy'] >= 0.8) & (df['accuracy'] <= 1.0)]
+    st.line_chart(data=filtered_df, x='epoch', y='accuracy', use_container_width=True, height=400)
+    
+    st.write("Mean IoU per epoch")
+    filtered_df = df[(df['mean_iou'] >= 0.8) & (df['mean_iou'] <= 1.0)]
+    st.line_chart(data=filtered_df, x='epoch', y='mean_iou', use_container_width=True, height=400)
+    
     st.write("Loss per epochs")
-    st.line_chart(data=df, x='epoch', y='loss')
-if __name__ == "__main":
+    st.line_chart(data=df, x='epoch', y='loss', use_container_width=True, height=400)
+
+if __name__ == "__main__":
     main()
+
+
+#sudo su
+#yum install git
+#git clone https://github.com/Leo-Varin/CloudComputing.git
+#yum install python3-pip
+#python3 -m pip install --ignore-installed streamlit
+#python3 -m streamlit run app.py
